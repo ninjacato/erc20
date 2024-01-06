b2767ca065c0905f9bc47b5fc018cd5419bb8cc5
ninjacato <cato@firi.com>
2024-01-06 14:50:02 +0100

    Re-export web3 types

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,21 @@
 
 extern crate serde;
 
-mod error;
-/// A set of useful methods and abstractions.
-pub mod util;
-#[cfg(test)]
-mod util_tests;
-/// Ethereum transfer abstraction.
-pub mod transfer;
 /// ERC20 specific information.
 pub mod erc20;
 #[cfg(test)]
 mod erc20_tests;
+mod error;
 /// web3 transaction specific operations.
 pub mod transaction;
 #[cfg(test)]
 mod transaction_tests;
+/// Ethereum transfer abstraction.
+pub mod transfer;
+/// A set of useful methods and abstractions.
+pub mod util;
+#[cfg(test)]
+mod util_tests;
 
 pub use self::error::ERC20Error;
+pub use web3::types::{Index, Transaction, H160, H256, U256, U64};
